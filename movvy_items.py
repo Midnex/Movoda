b20c190f08cfe4352e6874b6677f4ae195b82d2e
Duckabush <llanerost@gmail.com>
2019-06-11 16:25:35 -0700

    major changes

--- movvy_items.py
+++ movvy_items.py
@@ -1,118 +1,210 @@
-import csv, datetime, pyperclip
-
-# TODO: make a read database function to elimate extra code.
-#       Search by location
-#       Update to only show newest data for any given store, instead of all.
-#       Fuzzy and Strict Search, with quotes "" to do strict, allow store, guild, player, item or location results, specify buy or sell, maybe sort buy/sell.
-
-def add_to_file(add_to_file):
-    stamp = datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S")
-    if add_to_file == 'clipboard':
-        toImport = pyperclip.paste()
-        newList = toImport.split('\n')
-        fullLine = []
-
-        with open('itemDB.csv', 'a', encoding='utf-8', newline='') as itemDB:
-            itemDB_writer = csv.writer(itemDB, quoting=csv.QUOTE_ALL)
-            for line in newList:
-                if len(line.split('\t')) == 2:
-                    stamp = line.split('\t')[1].replace('\t','')
-                if ' in house ' in line: # for now just removes house scans.
-                    location = line[0:line.find('-')-1]
-                    clan = ''
-                    type = 'house'
-                    item = ''
-                    price = ''
-                    store = line[line.find(' in house ')+10:]
-                else:
-                    location = line[0:line.find('-')-1]
-                    clan = line[line.find('-')+2:line.find(' is ')]
-                    if ' is buying ' in line:
-                        type = 'buy'
-                        item = line[line.find(' is buying ')+11:line.find(' for ')]
-                    else:
-                        type = 'sell'
-                        item = line[line.find(' is selling ')+12:line.find(' for ')]
-                    price = line[line.find(' for ')+5:line.find(' in ')].replace('V','').replace(',','')
-                    store = line[line.find(' in ')+4:line.find('\t')]
-                    itemDB_writer.writerow([stamp,location,clan,type,item,price,store])
-        itemDB.close()
-        print('Added finder spell list to Database')
-    elif add_to_file == 'store':
-        newList = pyperclip.paste().split('\n')
-        location = input('Location: ')
-        clan = input('Guild: ')
-        store = ''
-        item = ''
-        price = ''
-        type = ''
-        for i in newList:
-        	if i == newList[0]:
-        		store = newList[0]
-        	elif ' for ' in i:
-        		type = 'buy'
-        		item = i[0:i.find(' for ')]
-        		price = i[i.find(' for ')+5:].replace('V','')
-        		print(stamp,location,clan,type,item,price,store)
-        	elif 'Buy' in i: ## needs to be fixed still
-        		type = 'sell'
-        		item = i[0:10]
-        		print(stamp,location,clan,type,item,price,store)
-        print('Added store list to Database')
-    elif add_to_file == 'auction':
-        print('this does not exist yet')
-
-    pyperclip.copy('')
-
-def lookUp(item):
-    if item == 'item':
-        # should probably sort, only print latest files, space the formatting into columns.
-        with open('itemDB.csv', 'r', encoding='utf-8') as itemDB:
-            itemName = input('Enter Item: ')
-            databaseReader = csv.DictReader(itemDB)
-            resultsFound = 0
-            for row in databaseReader:
-                "timestamp","location","clan","type","item","price","store"
-                if itemName.lower() in row['item'].lower():
-                    print(row['timestamp'].strip(),'-',row["type"],'-',row['price'] + 'V',row["clan"],'in',row["location"],row["item"],'at',row["store"] + '.')
-                    resultsFound += 1
-            if resultsFound == 0:
-                print('\n No results found for',itemName,'check spelling and try again')
-            elif resultsFound == 1:
-                print('\n',resultsFound, 'result found for',itemName + '.\n')
-            else:
-                print('\n',resultsFound, 'results found for',itemName + '.\n')
-        itemDB.close()
-        menuSystem()
-    if item == 'store':
-        print('Coming Soon!\n')
-        menuSystem()
-    if item == 'location':
-        print('Coming Soon!\n')
-        menuSystem()
-
+import csv
+from datetime import datetime
+import pyperclip
 
 def menuSystem():
-    menuItem = input('Choose a menu item:\n1 Add: Finder Spell Parse\n2 Add: Store Parse\n3 Add: Auction Parse\n4 Search: Item\n5 Search: Store\n6 Search: Location\n7 Exit\n')
-    if menuItem == '1': # Finder Spell Parse
-        add_to_file('clipboard')
+    ''' A menu! '''
+    print('Choose a menu number:')
+    menuSelections = [
+        '1. Add: Finder Spell',
+        '2. Add: Store Parse',
+        '3. Add: Scan Auction',
+        '4. Search: Item',
+        '5. Search: Store (fuzzy)',
+        '6. Search: Location',
+        '7. Exit'
+    ]
+    for item in menuSelections:
+        print(item)
+    menuItem = input()
+
+    if menuItem == '1':  # Finder Spell Parse
+        clipboard_finder_parse()
         menuSystem()
-    elif menuItem == '2': # Store Parse
-        add_to_file('store')
+    elif menuItem == '2':  # Store Parse
+        clipboard_store_parse()
         menuSystem()
-    elif menuItem == '3': # Auction Parse
-        add_to_file('auction')
+    elif menuItem == '3':  # Auction Parse
+        # auction_scan_parse()
         menuSystem()
-    elif menuItem == '4': # Search by Item Name (fuzzy)
-        lookUp('item')
-    elif menuItem == '5': # search by Store
-        lookUp('store')
-    elif menuItem == '6': # search by Location
-        lookUp('location')
-    elif menuItem == '7': # Exit
+    elif menuItem == '4':  # Search by Item Name (fuzzy)
+        searchQuery = input('Item to find: ')
+        lookUp(searchQuery, 'item')
+        menuSystem()
+    elif menuItem == '5':  # search by Store
+        searchQuery = input('Store to check: ')
+        lookUp(searchQuery, 'store')
+        menuSystem()
+    elif menuItem == '6':  # search by Location
+        while True:
+            searchQuery = input('Location to check: ')
+            if checkLocation(searchQuery) == True:
+                lookUp(searchQuery, 'location')
+                break
+        menuSystem()
+    elif menuItem == '7':  # Exit
         print('Exiting...')
     else:
-        print('\n"' + menuItem + '" is not a valid option.')
+        print(f'\n {menuItem} is not a valid option.')
         menuSystem()
+
+def checkLocation(location):
+    loc_list = ['Ashia', 'Awaru', 'Barin Plains', 'Baron Plains', 'Bulbas',
+                'Cardina', 'Cardina Valley', 'Cythe', 'Danycia', 'Droesar',
+                'Echtin', 'Eptile', 'Essrom', 'Ferboi', 'Galawi', 'Garando Mines',
+                'Giroc', 'Haldos Outpost', 'Hevalus Jungle', 'Hikori',
+                'HMS Halieutika', 'Irotho', 'Jiroka', 'Kimdar',
+                'Kolar Trading Post', 'Kudzum', 'Lake Essdar', 'Lake Trand',
+                'Marossa', 'Martral', 'Moskim', 'Mount Pharos', 'Nalurn Woods',
+                'Naton', 'Odude', 'Onnix', 'Pharos Peak', 'Ponat', 'Ponat Pier',
+                'Port Barin', 'Port Baron', 'Port Schow', 'Radom Woods', 'Ravel',
+                'Rissdra', 'Sepas', 'Therusia', 'Tropi', 'Unopos Mesa', 'Uzlea',
+                'Yisildor Bay', 'Zhyack']
+    if location in loc_list:
+        return True
+    else:
+        print(f'"{location}" does not exist')
+        return False
+
+def database_reader():
+    '''Loads data base into a variable to be used else where'''
+
+    database = open('itemDB.csv', 'r', encoding='utf-8')
+    return csv.DictReader(database)
+
+def datebase_writer(line_to_write):
+    '''writes to the data base using data in line_to_write'''
+
+    with open('itemDB.csv', 'a', encoding='utf-8', newline='') as itemDB:
+        itemDB_writer = csv.writer(itemDB, quoting=csv.QUOTE_ALL)
+        itemDB_writer.writerow(line_to_write)
+
+def checkBuildingType(line):
+    '''Checks if the building type is a house, or if the item is being bought or sold'''
+
+    if ' is buying ' in line:
+        return 'buy'
+    elif ' is selling ' in line:
+        return 'sell'
+    else:
+        return 'house'
+
+def getPrice(building_type, line):
+    '''Gets the price of the item that is being bought or sold'''
+    if building_type == 'buy':
+        return line.split(' is buying ')[1].split(' for ')[0].strip()
+
+    if building_type == 'sell':
+        return line.split(' is selling ')[1].split(' for ')[0].strip()
+
+def clipboard_finder_parse():
+    '''Grabs the data in the clipboard that matches the finder spell data, and parses it to add to the database'''
+
+    item = ''
+    line_to_write = ''
+    count = 0
+    for line in pyperclip.paste().split('\n'):
+        print(line)
+        try:
+            if len(line.split(' results for ')) == 2:
+                item = line.split(' results for ')[1]
+        except:
+            pass
+        stamp = datetime.now().strftime("%m/%d/%y %H:%M:%S")
+        try:
+            building_location = line.split('-')[0].strip()
+            building_clan = line.split('-')[1].split(' is ')[0].strip()
+            building_type = checkBuildingType(line)
+            building_item = getPrice(building_type, line)
+            building_item_price = line.split(' for ')[1].split(' in ')[
+                0].strip()[:-1]
+            building_name = line.split(' in ')[1].strip()
+
+            line_to_write = [stamp, building_location, building_clan,
+                             building_type, building_item, building_item_price, building_name]
+            datebase_writer(line_to_write)
+            count += 1
+        except:
+            pass
+    if item == '':
+        print(f'Added {count} items at {stamp}\n')
+    else:
+        print(f'Added {count} items at {stamp} while searching for {item}\n')
+    pyperclip.copy('')
+
+def clipboard_store_parse():
+    '''Grabs the data in the clipboard that matches store data, and parses it to add to the data base'''
+
+    while True:
+            location = input('Location Name: ')
+            if checkLocation(location) == True:
+                break
+    clan = input('Clan: ')
+    line_to_write = ''
+    building_name = ''
+    count = 0
+    stamp = datetime.now().strftime('%m/%d/%y %H:%M:%S')
+    for line in pyperclip.paste().split('\n'):
+        building_item = ''
+        building_price = ''
+        building_type = ''
+        buy_sep = ' for '
+        sell_sep = '\t'
+        if count == 0:
+            building_name = line.replace('(Change Name)', '').strip()
+        if count > 1:
+            if buy_sep in line:
+                building_item = line.split(buy_sep)[0].replace(
+                    'Building Administration', '').strip()
+                building_price = line.split(buy_sep)[1].replace(
+                    'V', '').replace(',', '').strip()
+                building_type = 'buy'
+            if sell_sep in line:
+                building_item = line.split(sell_sep)[0].replace(
+                    'Building Administration', '').strip()
+                building_price = line.split(sell_sep)[1].replace(
+                    'V', '').replace(',', '').strip()
+                building_type = 'sell'
+            if building_type != '':
+                line_to_write = [stamp, location, clan, building_type,
+                                 building_item, building_price, building_name]
+                datebase_writer(line_to_write)
+        count += 1
+    print(f'Added {count} items at {stamp} from {location} - {clan} - {building_name}\n')
+
+def auction_scan_parse():
+    '''Once done will scan the auction house and new building type called auction'''
+    return
+
+def lookUp(searchQuery, term):
+    '''Looks up items based on item, store or location'''
+
+    searchQuery = searchQuery
+    resultsFound = 0
+    for line in database_reader():
+        timestamp = line['timestamp']
+        itemType = line['type']
+        price = line['price']
+        clan = line['clan']
+        location = line['location']
+        item = line['item']
+        store = line['store']
+        results = f'{timestamp} - {clan} is {itemType}ing {item} for {price}V in {location} at {store}.'
+        if term == 'item':
+            if searchQuery.lower() == item.lower():
+                print(results)
+                resultsFound += 1
+        if term == 'store':
+            if searchQuery.lower() in store.lower():
+                print(results)
+                resultsFound += 1
+        if term == 'location':
+            if searchQuery.lower() == location.lower():
+                print(results)
+                resultsFound += 1
+    if resultsFound == 0:
+        print(f'\n No result(s) found for "{searchQuery}" check spelling and try again.')
+    else:
+        print(f'\n {resultsFound} result(s) found for {searchQuery}.\n')
 
 menuSystem()
