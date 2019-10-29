a9ed52c893d2513d8c943ea4c0da525cb2406ff5
Duckabush <llanerost@gmail.com>
2019-10-29 16:25:20 -0700

    initial commit

--- convert_to_mongo.py
+++ convert_to_mongo.py
@@ -1,20 +1,23 @@
-# import csv
-# from pymongo import MongoClient
-# client = MongoClient('mongodb://localhost:27017')
+import csv
+import pymongo
+import config
+from pymongo import MongoClient
 
-# db = client.movoda
+client = MongoClient(config.credentials)
+db = client.movoda
+prices = db.prices
 
-# prices = db.prices
-
-# with open('itemDB.csv', 'r') as f:
-#     fcsv = csv.DictReader(f)
-#     for line in fcsv:
-#         prices_data = {
-#             'timestamp': line['timestamp'],
-#             'location': line['location'],
-#             'clan': line['clan'],
-#             'type': line['type'],
-#             'item': line['item'],
-#             'price': line['price'],
-#             'store': line['store']}
-#         result = prices.insert_one(prices_data)
+with open('itemDB.csv', 'r') as f:
+    fcsv = csv.DictReader(f)
+    for i, line in enumerate(fcsv):
+        print(f"Processing Line: {i + 1}")
+        prices_data = {
+            'timestamp': line['timestamp'],
+            'location': line['location'],
+            'clan': line['clan'],
+            'type': line['type'],
+            'item': line['item'],
+            'price': line['price'],
+            'store': line['store']}
+        result = prices.insert_one(prices_data)
+client.close()
