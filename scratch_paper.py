9067c84e4260759c6936e77ac1b412cc25a42a96
Duckabush <llanerost@gmail.com>
2019-10-29 16:28:06 -0700

    temp file

--- scratch_paper.py
+++ scratch_paper.py
@@ -1,1 +1,22 @@
 # scratch paper for upcoming features/changes
+
+import config
+from tabulate import tabulate
+from pymongo import MongoClient
+
+client = MongoClient(config.credentials)
+db = client.movoda
+
+itemslist = db.prices
+
+data = []
+i2f = input('item: ')
+t2f = input('Buy or Sell: ')
+
+
+for i in itemslist.find({'item':i2f}):
+    if i['type'] == t2f.lower():
+        table = i['timestamp'],i['location'],i['clan'],i['type'],i['item'],i['price'],i['store']
+        data.append(table)
+
+print(tabulate(data, headers=['timestamp','location','clan','type','item','price','store'], tablefmt='psql'))
