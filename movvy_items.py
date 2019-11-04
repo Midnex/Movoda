b51b0020c4d34c2610f604a0ce24b6baf970d664
Duckabush <llanerost@gmail.com>
2019-11-03 23:46:56 -0800

    changes to database
    Moved database over to mongodb. Still a few things to change.

--- movvy_items.py
+++ movvy_items.py
@@ -2,22 +2,32 @@ import config
 import csv
 import pyperclip
 from datetime import datetime
+<<<<<<< Updated upstream
+=======
+from tabulate import tabulate
+from pymongo import MongoClient
+
+client = MongoClient(config.credentials)
+db = client.movoda
+prices = db.prices
+locations = db.locations
+>>>>>>> Stashed changes
 
 ver = '0.61'
 
 def menuSystem():
     print('Choose a menu selection:')
     menuSelections = (
-        '  1. Add: Finder Spell',
-        '  2. Add: Store Parse',
-        '  3. Add: Scan Auction',
-        '  4. Search: Item',
-        '  5. Search: Store (fuzzy)',
-        '  6. Search: Location',
-        '  7. Exit'
+        'Add: Finder Spell',
+        'Add: Store Parse',
+        'Add: Scan Auction',
+        'Search: Item',
+        'Search: Store (Strict)',
+        'Search: Location (Broke)',
+        'Exit'
     )
-    for item in menuSelections:
-        print(item)
+    for num, item in enumerate(menuSelections, start=1):
+        print(f"  {num}. {item}")
     menuItem = input('\n > ')
 
     if menuItem == '1':  # Finder Spell Parse
@@ -43,7 +53,7 @@ def menuSystem():
         while True:
             search = input('Location to check: ')
             searchQuery = search.split(',')[0]
-            if checkLocation(searchQuery) == True:
+            if checkLocation_new(searchQuery) == True:
                 lookUp(searchQuery, 'location')
                 break
         menuSystem()
@@ -53,41 +63,55 @@ def menuSystem():
         print(f'\n {menuItem} is not a valid option.')
         menuSystem()
 
+def checkLocation_new(location):
+    for i in locations.find({'name':location}):
+        if i['name'] == location:
+            return True
+        else:
+            print(f'{location} does not exist.')
+            return False
 
-def checkLocation(location):
-    ''' Pass a location to check if it is a valid game location.'''
-    loc_list = ['Ashia', 'Awaru', 'Barin Plains', 'Baron Plains', 'Bulbas',
-                'Cardina', 'Cardina Valley', 'Cythe', 'Danycia', 'Droesar',
-                'Echtin', 'Eptile', 'Essrom', 'Ferboi', 'Galawi', 'Garando Mines',
-                'Giroc', 'Haldos Outpost', 'Hevalus Jungle', 'Hikori',
-                'HMS Halieutika', 'Irotho', 'Jiroka', 'Kimdar',
-                'Kolar Trading Post', 'Kudzum', 'Lake Essdar', 'Lake Trand',
-                'Marossa', 'Martral', 'Moskim', 'Mount Pharos', 'Nalurn Woods',
-                'Naton', 'Odude', 'Onnix', 'Pharos Peak', 'Ponat', 'Ponat Pier',
-                'Port Barin', 'Port Baron', 'Port Schow', 'Radom Woods', 'Ravel',
-                'Rissdra', 'Sepas', 'Therusia', 'Tropi', 'Unopos Mesa', 'Uzlea',
-                'Yisildor Bay', 'Zhyack']
-    if location in loc_list:
-        return True
+def database_reader_new(searchQuery, queryItemType, term):
+    data = []
+    searchQuery = ''.join(searchQuery)
+    if term == 'store':
+        for i in prices.find({'store': searchQuery}):
+            table = i['timestamp'], i['location'], i['clan'], i['type'], i['item'], int(i['price']), i['store']
+            data.append(table)
+    elif term == 'item':
+        for i in prices.find({'item': searchQuery}):
+            if i['type'] == queryItemType.lower():
+                table = i['timestamp'], i['location'], i['clan'], i['type'], i['item'], int(i['price']), i['store']
+                data.append(table)
+            elif queryItemType == 'both':
+                table = i['timestamp'], i['location'], i['clan'], i['type'], i['item'], int(i['price']), i['store']
+                data.append(table)
+    elif term == 'location':
+        for i in prices.find({'location': searchQuery}):
+            table = i['timestamp'], i['location'], i['clan'], i['type'], i['item'], int(i['price']), i['store']
+            data.append(table)
+
+    if len(data) == 0:
+        print(f"No result found for {searchQuery}.\n")
     else:
-        print(f'{location} does not exist.')
-        return False
+        if queryItemType == 'sell':
+            sorted_data = sorted(data, key=lambda x: x[5], reverse=True)
+        elif queryItemType == 'buy':
+            sorted_data = sorted(data, key=lambda x: x[5], reverse=False)
+        else:
+            sorted_data = data
+        return sorted_data
 
+<<<<<<< Updated upstream
+=======
+def database_writer_new(line_to_write):
+    '''writes to mongodb using data in line_to_write (list)'''
 
-def database_reader():
-    '''Loads data base into a variable to be used else where'''
+    timestamp, location, clan, queryItemType, item, price, store = line_to_write
+    data = {'timestamp': timestamp,'location': location,'clan': clan,'type': queryItemType,'item': item,'price': price,'store': store}
+    result = prices.insert_one(data)
 
-    database = open('itemDB.csv', 'r', encoding='utf-8')
-    return csv.DictReader(database)
-
-
-def datebase_writer(line_to_write):
-    '''writes to the data base using data in line_to_write'''
-
-    with open('itemDB.csv', 'a', encoding='utf-8', newline='') as itemDB:
-        itemDB_writer = csv.writer(itemDB, quoting=csv.QUOTE_ALL)
-        itemDB_writer.writerow(line_to_write)
-
+>>>>>>> Stashed changes
 
 def checkBuildingType(line):
     '''Checks if the building type is a house, or if the item is being bought or sold'''
@@ -116,7 +140,6 @@ def clipboard_finder_parse():
     line_to_write = ''
     count = 0
     for line in pyperclip.paste().split('\n'):
-        print(line)
         try:
             if len(line.split(' results for ')) == 2:
                 item = line.split(' results for ')[1]
@@ -148,7 +171,7 @@ def clipboard_store_parse():
 
     while True:
             location = input('Location Name: ')
-            if checkLocation(location) == True:
+            if checkLocation_new(location) == True:
                 break
     clan = input('Clan: ')
     line_to_write = ''
@@ -187,48 +210,14 @@ def auction_scan_parse():
 def lookUp(searchQuery, term):
     '''Looks up items based on item, store or location'''
     if len(searchQuery) == 2:
-        queryItem, queryItemType = searchQuery
-        queryItem = queryItem.strip()
-        queryItemType = queryItemType.strip()
+        queryItem = searchQuery[0].strip()
+        queryItemType = searchQuery[1].strip()
     else:
-        queryItem = ''.join(searchQuery)
-    resultsFound = 0
-    data = []
-    for line in database_reader():
-        timestamp = line['timestamp']
-        itemType = line['type']
-        price = line['price']
-        clan = line['clan']
-        location = line['location']
-        item = line['item']
-        store = line['store']
-        results = f'{timestamp} - {clan} is {itemType}ing {item} for {price}V in {location} at {store}.'
-        if term == 'item':
-            if queryItem.lower() == item.lower() and queryItemType.lower() == itemType:
-                data.append(results)
-                resultsFound += 1
-            elif queryItem.lower() == item.lower() and queryItemType.lower() == 'both':
-                data.append(results)
-                resultsFound += 1
-        if term == 'store':
-            if queryItem.lower() in store.lower():
-                data.append(results)
-                resultsFound += 1
-        if term == 'location':
-            if queryItem.lower() == location.lower():
-                data.append(results)
-                resultsFound += 1
-    if resultsFound == 0:
-        print(
-            f'\n No result(s) found for "{queryItem}" check spelling and try again.')
-    else:
-        printResults(data)
-        print(f'\n {resultsFound} result(s) found for {queryItem}.\n')
-
-
-def printResults(results):
-    for line in results:
-        print(line)
+        queryItem = searchQuery
+        queryItemType = 'both'
+    sorted_data = database_reader_new(queryItem, queryItemType, term)
+    if sorted_data != None:
+        print(tabulate(sorted_data, headers=['timestamp', 'location', 'clan', 'type', 'item', 'price', 'store'], tablefmt='psql'),'\n')
 
 if __name__ == '__main__':
     menuSystem()
